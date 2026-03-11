d5c1713a4f21cb9130a6977c9f3b219dc2657b43
brooksmesser <bm536931@georgetownisd.org>
2026-03-11 08:03:07 -0500

    Update challenge_two.py

--- challenge_two.py
+++ challenge_two.py
@@ -56,4 +56,50 @@ PROCEDURE shop_buy(inventoryMoney, inventoryPick, stockList) {
 
 def shop_buy(stock, inventory):
     # Write your translated Python code below this line!
-    pass
+    def shop_buy(inventoryMoney, inventoryPick, stockList):
+    keepShopping = "y"
+
+    while keepShopping == "y" or keepShopping == "Y":
+        print("--- Items for Sale ---")
+
+        for i, item in enumerate(stockList, start=1):
+            print(f"{i}. {item[0]} - ${item[1]}")
+
+        choice = input("Enter the number of the item you want to buy: ")
+
+        if not choice.isdigit():
+            print("Sorry, we don't have that.")
+            keepShopping = input("Keep shopping? Y/N: ")
+            continue
+
+        choice = int(choice)
+
+        if 1 <= choice <= len(stockList):
+            selectedItem = stockList[choice - 1]
+
+            itemName = selectedItem[0]
+            itemPrice = selectedItem[1]
+
+            if inventoryMoney >= itemPrice:
+                inventoryMoney -= itemPrice
+
+                substring = itemName[9:12]
+
+                if substring == "Enh":
+                    inventoryPick = "good"
+                elif substring == "Gre":
+                    inventoryPick = "better"
+                elif substring == "Sup":
+                    inventoryPick = "best"
+
+                stockList.pop(choice - 1)
+
+                print(f"You bought the {itemName}!")
+            else:
+                print("You can't afford that!")
+        else:
+            print("Sorry, we don't have that.")
+
+        keepShopping = input("Keep shopping? Y/N: ")
+
+    return inventoryMoney
